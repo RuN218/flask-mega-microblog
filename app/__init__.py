e2c766659d0fa41476628effe0c0ed167e5f1831
Ruslan <218.run@gmail.com>
2019-09-13 16:19:41 +0300

    User Logins

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -6,7 +7,11 @@ from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+login = LoginManager(app)
+login.login_view = "login"
 
 from app import routes, models
