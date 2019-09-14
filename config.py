2192b7041dacfdc4a2bb6cf5b1bd7fc25adf557d
Ruslan <218.run@gmail.com>
2019-09-14 21:22:36 +0300

    Pagination

--- config.py
+++ config.py
@@ -16,3 +16,5 @@ class Config(object):
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["218.run@gmail.com"]
+
+    POSTS_PER_PAGE = 3
