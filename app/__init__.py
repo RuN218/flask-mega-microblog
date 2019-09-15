49af8c282ea1fa0720d1a1781ae4a5005dc8a671
Ruslan <218.run@gmail.com>
2019-09-15 11:41:30 +0300

    Dates and Times

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 from config import Config
 
@@ -20,6 +21,8 @@ migrate = Migrate(app, db)
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
+
+moment = Moment(app)
 
 login = LoginManager(app)
 login.login_view = "login"
