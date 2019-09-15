d0b91baf6262b5cc827ddc6fca517435dffc0edd
Ruslan <218.run@gmail.com>
2019-09-15 11:09:23 +0300

    Facelift

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 from config import Config
 
@@ -17,6 +18,8 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 login = LoginManager(app)
 login.login_view = "login"
