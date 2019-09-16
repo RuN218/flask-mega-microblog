d84cd7c887f6ebb3e5ccf1de10b2d670cfc90b62
Ruslan <218.run@gmail.com>
2019-09-16 17:37:06 +0300

    A Better Application Structure

--- app/models.py
+++ app/models.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from hashlib import md5
 from time import time
-import jwt
 
+import jwt
+from flask import current_app
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
-from app import app, db, login
+from app import db, login
 
 
 @login.user_loader
@@ -74,12 +75,12 @@ class User(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {"reset_password": self.id, "exp": time() + expires_in},
-            app.config["SECRET_KEY"], algorithm="HS256").decode("utf-8")
+            current_app.config["SECRET_KEY"], algorithm="HS256").decode("utf-8")
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config["SECRET_KEY"],
+            id = jwt.decode(token, current_app.config["SECRET_KEY"],
                             algorithms=["HS256"])["reset_password"]
         except:
             return
--- app/translate.py
+++ app/translate.py
@@ -1,19 +1,19 @@
 import json
 import requests
 
-from app import app
+from flask import current_app
 from flask_babel import _
 
 
 def translate(text, source_language, dest_language):
-    if 'YA_TRANSLATOR_KEY' not in app.config or \
-            not app.config['YA_TRANSLATOR_KEY']:
+    if 'YA_TRANSLATOR_KEY' not in current_app.config or \
+            not current_app.config['YA_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
 
     # auth = {'Ocp-Apim-Subscription-Key': app.config['YA_TRANSLATOR_KEY']}
     r = requests.get(
         f"""https://translate.yandex.net/api/v1.5/tr.json/translate
-        ? [key={app.config['YA_TRANSLATOR_KEY']}]
+        ? [key={current_app.config['YA_TRANSLATOR_KEY']}]
         & [text={text}]
         & [lang={source_language}-{dest_language}]"""
     )
