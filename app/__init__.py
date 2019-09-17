0c9d93356c3c08f991e20163e3d4820485440965
Ruslan <218.run@gmail.com>
2019-09-17 18:52:25 +0300

    User Notifications

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ from elasticsearch import Elasticsearch
 from config import Config
 
 db = SQLAlchemy()
-migrate = Migrate(db)
+migrate = Migrate()
 
 login = LoginManager()
 login.login_view = "auth.login"
@@ -32,7 +32,7 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
 
     db.init_app(app)
-    migrate.init_app(app)
+    migrate.init_app(app, db)
     login.init_app(app)
     mail.init_app(app)
     bootstrap.init_app(app)
